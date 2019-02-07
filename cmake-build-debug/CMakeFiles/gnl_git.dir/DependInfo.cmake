062e8637032486040d372a5056787de2f330a8d0
Cat Braddinton <katrinmclawren@gmail.com>
2019-02-07 21:13:21 +0200

    works. pushing to check for leaks. not refactored. norm fail

---
+++ cmake-build-debug/CMakeFiles/gnl_git.dir/DependInfo.cmake
@@ -0,0 +1,86 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/kdudko/Desktop/gnl_git/get_next_line.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/get_next_line.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_atoi.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_atoi.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_bzero.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_bzero.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_count_words.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_count_words.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_isalnum.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_isalnum.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_isalpha.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_isalpha.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_isascii.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_isascii.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_isdigit.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_isdigit.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_isprint.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_isprint.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_isspace.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_isspace.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_itoa.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_itoa.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_lstadd.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_lstadd.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_lstdel.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_lstdel.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_lstdelone.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_lstdelone.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_lstiter.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_lstiter.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_lstmap.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_lstmap.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_lstnew.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_lstnew.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_memalloc.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_memalloc.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_memccpy.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_memccpy.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_memchr.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_memchr.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_memcmp.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_memcmp.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_memcpy.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_memcpy.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_memdel.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_memdel.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_memmove.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_memmove.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_memset.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_memset.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_putchar.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_putchar.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_putchar_fd.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_putchar_fd.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_putendl.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_putendl.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_putendl_fd.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_putendl_fd.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_putnbr.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_putnbr.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_putnbr_fd.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_putnbr_fd.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_putstr.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_putstr.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_putstr_fd.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_putstr_fd.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_search_and_replace.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_search_and_replace.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strcat.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strcat.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strchr.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strchr.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strclr.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strclr.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strcmp.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strcmp.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strcpy.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strcpy.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strdel.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strdel.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strdup.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strdup.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strequ.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strequ.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_striter.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_striter.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_striteri.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_striteri.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strjoin.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strjoin.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strlcat.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strlcat.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strlcpy.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strlcpy.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strlen.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strlen.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strmap.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strmap.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strmapi.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strmapi.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strncat.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strncat.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strncmp.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strncmp.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strncpy.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strncpy.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strndup.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strndup.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strnequ.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strnequ.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strnew.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strnew.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strnstr.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strnstr.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strrchr.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strrchr.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strsplit.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strsplit.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strstr.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strstr.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strsub.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strsub.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_strtrim.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_strtrim.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_tolower.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_tolower.c.o"
+  "/Users/kdudko/Desktop/gnl_git/libft/ft_toupper.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/libft/ft_toupper.c.o"
+  "/Users/kdudko/Desktop/gnl_git/main.c" "/Users/kdudko/Desktop/gnl_git/cmake-build-debug/CMakeFiles/gnl_git.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../libft"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- get_next_line.c
+++ get_next_line.c
@@ -12,39 +12,80 @@
 
 #include "get_next_line.h"
 
-int    buff_proccessing(char *buff, char *line)
+static void    *ft_realloc(void *ptr, size_t size)
 {
-    char *str;
-    int i;
+    void    *new_ptr;
 
-    str = (char *)malloc((BUFF_SIZE + 1) * sizeof(char));
-    i = 0;
-    while (i < BUFF_SIZE)
+    if (!ptr)
+        return (new_ptr = ft_memalloc(size));
+    else if (!size && ptr)
     {
-        str[i] = buff[i];
-        if (str[i] == 13)
-        {
-            str[i] = '\0';
-            line = (char *) malloc(ft_strlen(str) * sizeof(char));
-            line = ft_strcpy(line, str);
-            i = 0;
+        ft_memdel(&ptr);
+        return (new_ptr = ft_memalloc(1));
+    }
+    else if (!(new_ptr = ft_memalloc(size)))
+        return (NULL);
+    new_ptr = ft_memmove(new_ptr, ptr, size);
+    ft_memdel(&ptr);
+    return (new_ptr);
+}
+
+int             get_next_line(int fd, char **line)
+{
+    static int      gnl_calls_counter;
+    char            buf[2];
+    int             loop_counter;
+
+    if (fd)
+    {
+        loop_counter = 1;
+        if (gnl_calls_counter == 0)
+        {   
+            gnl_calls_counter = 1;
+            if (*line)
+                free(*line);
+            if (!(*line = ft_memalloc(BUFF_SIZE + 1)))
+                return (-1);
         }
-        i++;
+        while (loop_counter <= BUFF_SIZE)
+        {
+            read(fd, buf, 1);
+            if (buf[0] == 0)
+                return (0);
+            else if (buf[0] == '\n')
+            {
+                gnl_calls_counter = 0;
+                return (1);
+            }
+            ft_strlcat(*line, buf, BUFF_SIZE * gnl_calls_counter);
+            loop_counter++;
+        }
+        
+        if (loop_counter > BUFF_SIZE)
+        {
+            gnl_calls_counter++;
+            *line = (char *)ft_realloc(*line, (loop_counter * gnl_calls_counter));
+            get_next_line(fd, &*line);
+            return (1);
+        }
     }
-    if (i != BUFF_SIZE)
-        return (1);
+    return (-1);
 }
 
-int     get_next_line(const int fd, char **line)
-{
-	static char buff[BUFF_SIZE];
-	
-	if (fd)
-    {
-	    read(fd, buff, BUFF_SIZE);
-	    if ((buff_proccessing(buff, &line)) == 1)
-	        return (1);
+/*
+// printf("gnl started ... \n");
+  // printf("if first time ... \n");
+// printf("line first time: %s\n", *line);
+// printf("loop n: %d,\nbuff[i] = %c\n", loop_count, buf[0]);
+ // printf("%s\n", *line);
+                // printf("%s\n", *line);
 
-    }
-	return (-1);
-}
+                // printf("line len = %zu\nreturning to main ...\n", strlen(*line));
+                // printf("real_count1 = %d\n", real_count);
+
+            // printf("buf[0] = %c &&  line = %s\n", buf[0], *line);
+            // printf("line len before realloc: %zu\n", strlen(*line));
+            // printf("after realloc new_line: %s\n", *line);
+            // printf("line len after realloc: %zu\n", strlen(*line));
+            // printf("error openning file\n");
+ */
